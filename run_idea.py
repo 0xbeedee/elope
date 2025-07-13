db7fa06eff75891bb34a0815a71b077f131fc4c6
de-lachende-cavalier <81985269+de-lachende-cavalier@users.noreply.github.com>
2025-07-13 14:18:20 +0200

    general refactor

--- run_idea.py
+++ run_idea.py
@@ -1,23 +1,28 @@
 import argparse
 
-from ideas import *
+from ideas.idea_one.idea import IdeaOne
+
+IDEA_MAP = {
+    "one": IdeaOne,
+}
 
 
 def main(idea_name: str) -> None:
-    idea_map = {
-        "one": IdeaOne,
-    }
-
-    if idea_name not in idea_map:
-        print(
-            f"[+] Idea name not recognised! Available names are: {list(idea_map.keys())}"
+    if idea_name not in IDEA_MAP:
+        raise KeyError(
+            f"[!] Idea name not recognised. Available names are: {list(IDEA_MAP.keys())}"
         )
-        return
 
-    # TODO this is kind of ugly, perhaps use functions instead of classes?
-    idea = idea_map[idea_name]
-    idea()
-    idea.run()
+    idea = IDEA_MAP[idea_name]()
+    print(
+        f"[+] {idea.__class__.__name__}: Preprocessing the trainining and the test data..."
+    )
+    idea.preprocess_data()
+    # TODO what if other ideas do not involve training a neural net?
+    print(f"[+] {idea.__class__.__name__}: Training the neural net...")
+    # TODO keep the configuration for the various ideas into separate YAMLs
+    idea.train_net()
+    # idea.run()
 
 
 if __name__ == "__main__":
