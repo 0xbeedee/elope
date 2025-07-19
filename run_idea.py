152f40ac697e857598e704184a72badc2e84bca4
de-lachende-cavalier <81985269+de-lachende-cavalier@users.noreply.github.com>
2025-07-19 15:09:58 +0200

    added hydra for improved management of experiments

--- run_idea.py
+++ run_idea.py
@@ -1,4 +1,5 @@
-import argparse
+import hydra
+from omegaconf import DictConfig
 
 from ideas.idea_one.idea import IdeaOne
 
@@ -7,36 +8,24 @@ IDEA_MAP = {
 }
 
 
-def main(idea_name: str) -> None:
+@hydra.main(version_base=None, config_path="conf", config_name="config")
+def main(cfg: DictConfig) -> None:
+    idea_name = cfg.ideas.idea_name
     if idea_name not in IDEA_MAP:
         raise KeyError(
             f"[!] Idea name not recognised. Available names are: {list(IDEA_MAP.keys())}"
         )
 
-    idea = IDEA_MAP[idea_name]()
+    idea = IDEA_MAP[idea_name](config=cfg.ideas)
     print(
         f"[+] {idea.__class__.__name__}: Preprocessing the trainining and the test data..."
     )
     idea.preprocess_data()
     # TODO what if other ideas do not involve training a neural net?
     print(f"[+] {idea.__class__.__name__}: Training the neural net...")
-    # TODO keep the configuration for the various ideas into separate YAMLs
     idea.train_net()
     # idea.run()
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Run an idea.")
-    parser.add_argument(
-        "-n",
-        "--name",
-        type=str,
-        required=True,
-        help="Name of the idea to consider",
-        metavar="IDEA_NAME",
-    )
-
-    args = parser.parse_args()
-    main(
-        args.name,
-    )
+    main()
