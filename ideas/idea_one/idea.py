57cd1a80430ca08c423af28a1db4cf647bc8015d
de-lachende-cavalier <81985269+de-lachende-cavalier@users.noreply.github.com>
2025-07-27 14:38:59 +0200

    added better logging

--- ideas/idea_one/idea.py
+++ ideas/idea_one/idea.py
@@ -1,4 +1,5 @@
 from omegaconf import DictConfig
+from tqdm import tqdm
 
 from ideas.base import Idea
 from ideas.idea_one.nets.parallel_net import ParallelNet
@@ -6,6 +7,7 @@ from ideas.idea_one.utils import EventsTrajDataset, preprocess_data_streaming
 
 import torch
 from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
 
 # dict for mapping optimiser names to the correct classes
 OPTIM_MAP = {
@@ -42,17 +44,35 @@ class IdeaOne(Idea):
         train_dataset = EventsTrajDataset(self.train_data_path, shuffle=True)
         # set batch_size to 1 because event stacks have variable lengths
         train_loader = DataLoader(train_dataset, batch_size=1)
+
+        writer = SummaryWriter()
         for epoch in range(self.n_epochs):
-            self._one_step_train(train_loader)
+            epoch_loss, res_epoch_loss = 0, 0
+            with tqdm(
+                train_loader,
+                desc=f"\t[+] Epoch {epoch + 1}/{self.n_epochs}",
+                unit="batch",
+            ) as tqdm_ctxt:
+                acc_loss, res_acc_loss = self._one_epoch_train_(tqdm_ctxt)
+                epoch_loss += acc_loss
+                res_epoch_loss += res_acc_loss if res_acc_loss else 0
+
+            avg_loss = epoch_loss / len(train_loader)
+            writer.add_scalar("Loss/train", avg_loss, epoch)
+            print(f"\t[+] Epoch {epoch + 1} - Average Loss: {avg_loss:.5f}")
 
     def run(self) -> None:
         test_dataset = EventsTrajDataset(self.test_data_path)
         # TODO save output in desired JSON format
         pass
 
-    def _one_step_train(self, train_loader: DataLoader) -> None:
+    def _one_epoch_train_(self, tqdm_ctxt: tqdm) -> None:
+        """Trains the network for a single epoch (i.e., a single pass over all the trianing data).
+
+        The final underscore indicates that this function modifies its inputs as a side-effect. In this case, this is done to update the `tqdm_ctxt`.
+        """
         acc_samples, acc_labels = [], []
-        for X_batch, y_batch in train_loader:
+        for X_batch, y_batch in tqdm_ctxt:
             acc_samples.append(X_batch)
             acc_labels.append(y_batch)
             if len(acc_samples) == self.conf["acc_steps"]:
@@ -63,6 +83,9 @@ class IdeaOne(Idea):
         if acc_samples:
             # handle the remaining samples
             res_acc_loss = self._acc_batch_train(acc_samples, acc_labels)
+        tqdm_ctxt.set_postfix({"loss": acc_loss, "res_los": res_acc_loss})
+
+        return acc_loss, res_acc_loss
 
     def _acc_batch_train(self, samples: list, labels: list) -> float:
         """Processes a single accumulated batch of data."""
